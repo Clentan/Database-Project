a3853fd3b010488561f0f138e193d89d3b7457b2
Clentan <clentanchauke9089@gmail.com>
2025-02-16 20:23:54 +0200

    ...

---
+++ advanced-database-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-gray-700">
+    <p
+      className="first-letter:float-left first-letter:mr-3 first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 first-line:tracking-widest first-line:uppercase"
+    >
+      Well, let me tell you something, funny boy. Y'know that little stamp, the one that says "New York Public Library"?
+    </p>
+    <p className="mt-6">Well that may not mean anything to you, but that means a lot to me. One whole hell of a lot.</p>
+  </div>
+  );
+}
+
+export default App;
